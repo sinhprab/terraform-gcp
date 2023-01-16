a92e2b10d49dbcee59d324eb5c1c48826b26b650
sinhprab <sinha.pranshu2509@gmail.com>
2023-01-16 17:49:32 +0000

    Deploying my first simple GCP resource

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/Users/Pranshu/Downloads/pranshu-001-e1ea1a7da83e.json")
+
+  project = "pranshu-001"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
